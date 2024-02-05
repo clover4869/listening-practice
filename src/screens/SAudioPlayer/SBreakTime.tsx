539ddae106a87978e4fdb622b78ef87e54e604ed
clover <longtm02@gmail.com>
2024-02-05 13:05:26 +0700

    feat: implement library drag and drop

--- src/screens/SAudioPlayer/SBreakTime.tsx
+++ src/screens/SAudioPlayer/SBreakTime.tsx
@@ -1,11 +1,76 @@
 import * as React from 'react';
-import {Text, View, useWindowDimensions} from 'react-native';
+import {Text, TouchableOpacity, View, useWindowDimensions} from 'react-native';
+import {
+  NestableDraggableFlatList,
+  NestableScrollContainer,
+  ScaleDecorator,
+} from 'react-native-draggable-flatlist';
 import {TabView, SceneMap} from 'react-native-tab-view';
 
+const DATA = [
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+  {
+    key: 'jiji' + Math.random() * 100,
+    text: 'hihi' + Math.random() * 1000,
+  },
+];
+
 export default function SBreakTime() {
+  const [data, setData] = React.useState(DATA);
   return (
     <View style={{flex: 1, backgroundColor: '#673ab7'}}>
       <Text>SBreakTime</Text>
+      <NestableScrollContainer>
+        <NestableDraggableFlatList
+          data={data}
+          renderItem={({item, getIndex, drag, isActive}) => {
+            return (
+              <ScaleDecorator>
+                <TouchableOpacity
+                //   onLongPress={drag}
+                  onPressIn={drag}
+                  disabled={isActive}
+                  style={[{backgroundColor: isActive ? 'red' : 'blue'}]}>
+                  <View style={{height: 42}}>
+                    <Text>
+                      {getIndex()} hihi {item.text}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </ScaleDecorator>
+            );
+          }}
+          keyExtractor={item => item.key}
+          onDragEnd={({data}) => setData(data)}
+        />
+      </NestableScrollContainer>
     </View>
   );
 }
